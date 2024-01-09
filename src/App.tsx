75e030728e5588be07b2af1cc3ba11c6d1bfd601
Paulo <pvinics@gmail.com>
2024-01-09 20:08:15 -0300

    páginas de login e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes/routes";
+
 function App() {
     return (
         <>
-            <h1>Hello world</h1>
+            <RouterProvider router={routes} />
         </>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <div>
+            <p>API Alunos</p>
+        </div>
+    );
+};
