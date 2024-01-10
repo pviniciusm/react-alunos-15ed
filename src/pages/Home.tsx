1450aeb5e80e60a10e1b07b1ac53e805895e3284
Paulo <pvinics@gmail.com>
2024-01-09 22:30:18 -0300

    homepage com listagem de avaliações da daphne na API local

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
+import { ListaAvaliacoes } from "../components/ListaAvaliacoes";
+import { Avaliacao } from "../models/avaliacao.model";
+import axios from "axios";
 
 export const Home = () => {
+    const [avaliacoes, setAvaliacoes] = useState<Avaliacao[]>([]);
+
+    useEffect(() => {
+        listarAvaliacoes();
+    }, []);
+
+    const listarAvaliacoes = async () => {
+        try {
+            const result = await axios.get(
+                "http://localhost:3335/aluno/3a862521-db99-4d6d-9b0a-b16f56a1b7e6/avaliacao"
+            );
+
+            console.log(result.data.data);
+            setAvaliacoes(result.data.data);
+        } catch (error: any) {
+            setAvaliacoes([]);
+            console.log(error);
+            alert("A requisição deu erro!");
+        }
+    };
+
     return (
         <>
             <Header />
 
             <h1>Bem vindo!</h1>
             <h2>Lista de avaliações</h2>
+            <button onClick={listarAvaliacoes}>Atualizar lista</button>
 
-            <ul>
-                <li>Avaliação 1</li>
-                <li>Avaliação 2</li>
-            </ul>
+            <ListaAvaliacoes avaliacoes={avaliacoes} />
         </>
     );
 };
