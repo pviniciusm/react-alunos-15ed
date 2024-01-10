bb5dba98f7d7302eadeeba9e259a6a2dde5f2c46
Paulo <pvinics@gmail.com>
2024-01-09 22:29:39 -0300

    estilização do header com styled components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.div`
+    background-color: #0059a7;
+    padding: 16px 8px;
+    font-weight: bold;
+
+    p {
+        text-align: center;
+    }
+`;
+
 export const Header = () => {
     return (
-        <div>
+        <HeaderStyled>
             <p>API Alunos</p>
-        </div>
+        </HeaderStyled>
     );
 };
