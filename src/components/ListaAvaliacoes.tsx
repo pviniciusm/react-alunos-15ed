9a6a825623331a37d4b7ad9f80cdf4d395c73a4c
Paulo <pvinics@gmail.com>
2024-01-09 22:29:51 -0300

    component para listagem de avaliações

---
+++ src/components/ListaAvaliacoes.tsx
@@ -0,0 +1,21 @@
+import { Avaliacao } from "../models/avaliacao.model";
+
+interface ListaAvaliacoesProps {
+    avaliacoes: Avaliacao[];
+}
+
+export const ListaAvaliacoes = (props: ListaAvaliacoesProps) => {
+    console.log(props);
+
+    return (
+        <ul>
+            {props.avaliacoes.map((item) => {
+                return (
+                    <li key={item.id}>
+                        Disciplina: {item.disciplina} - Nota: {item.nota}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
